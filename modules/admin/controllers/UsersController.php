ca544529b4dd3ce92bb1b24142f581bec7082806
Alex Sivik <alexander.sivik@gmail.com>
2020-09-09 09:16:44 +0300

    Users

--- modules/admin/controllers/UsersController.php
+++ modules/admin/controllers/UsersController.php
@@ -6,17 +6,15 @@ use app\models\User;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 
-
 class UsersController extends Controller
 {
 	
-	
 	public function actionIndex()
-	{
-		
+	{   
+	    
 		$model = new User();
-		
-		
+		$this->setViewPath('C:\openserver\OSPanel\domains\blog.com\modules\admin\views\user');
+	
 		return $this->render('index',[
 		'model' => $model
 		]);
